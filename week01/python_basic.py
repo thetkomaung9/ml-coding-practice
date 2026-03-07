2cf2d45bd5a73607b9fb953c11f2fb554c554cda
이정빈 <jungbini@sunmoon.ac.kr>
2026-03-07 14:36:05 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -71,3 +71,18 @@ print(d.strip())
 
 # 문자열 수정 (불가능)
 a = "Pithon"
+# a[1] = 'y'    # 오류 발생
+
+# 문자열 바꾸기
+a = "Python is difficult."
+print(a.replace("diffcult", "funny"))
+print(a)
+
+# 문자열 나누기
+print(a.split())
+
+b = "a, b, c, d"
+print(b)
+print(b.split(','))
+
+# 리스트 만들기
