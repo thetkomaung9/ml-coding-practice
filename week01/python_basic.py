b39ccaf3da074ea08bf663485304fd8dcda6edaf
thetkomaung <thetkomaung966@gmail.com>
2026-03-11 01:02:06 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -302,3 +302,10 @@ def sum2(*args):
     for i in args:
         x += i
     return x
+#함수 호출
+a = 5
+b = 3
+print(sum1(a, b))
+print(sum1(3, 5))
+print(sum2(1, 2, 3, 4, 5))
+print(sum2(2, 3.5, 10))
