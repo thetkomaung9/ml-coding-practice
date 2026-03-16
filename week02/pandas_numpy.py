7a439db3ebcdaedf6c0fd23656a0fc5e4b7e2874
thetkomaung <thetkomaung966@gmail.com>
2026-03-17 01:44:23 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -39,4 +39,4 @@ print(sample_df.iloc[0:2])
 print(sample_df.iloc[0:3])
 print(sample_df.iloc[0:3, 2:4]) # 컴림까지 등시에 입댁싱
 
-print(sample_df.drop(['var_1', 'var_3']))
+print(sample_df.drop(['var_1', 'var_3'], axis=1)) #컬럼을 
