8c6d5ffdb343e0eaa9ae79204f51ffec1e030ac7
thetkomaung <thetkomaung966@gmail.com>
2026-03-17 01:13:53 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -15,4 +15,4 @@ a = pd.DateFrame(sample_dic)
 a.info()
 
 pd.DataFrame([[1,2,],[3,4], [5,6],[7,8]])
-pd.DataFrame([[1,2],[3,4],[5,6],[7,8]], columns= ['var_1', 'var_2'], index=)
+pd.DataFrame([[1,2],[3,4],[5,6],[7,8]], columns= ['var_1', 'var_2'], index=['a', 'b', 'c', 'd'])
