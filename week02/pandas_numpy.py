a055ae2fbb02062a177c2246af854719fcf064bd
thetkomaung <thetkomaung966@gmail.com>
2026-03-17 01:50:53 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -49,4 +49,7 @@ print(netflix.head())
 print(netflix['release_year'])
 print(netflix['release_year'] > 2015)
 
-more2015
+more2015 = netflix[netflix['release_year'] > 2015]
+print(more2015.head(10))
+
+print(~())
