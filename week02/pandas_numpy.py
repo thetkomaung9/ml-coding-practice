2822d9c0e7f02e87b2f02f95664650f034242a2f
thetkomaung <thetkomaung966@gmail.com>
2026-03-17 01:08:53 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample.csv'
-sample = pd.
+sample = pd.read_csv(file_url)
+
+print(sample.head)
+print(sample.tail())
+
+sample.inf0()
+sample.describe()
+
+sample_dic={'name': ['John', 'Ann', 'Kevin'],}
