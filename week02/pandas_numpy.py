1eca89d7e0211f15d8cacf2dcff913e890437218
thetkomaung <thetkomaung966@gmail.com>
2026-03-17 01:28:23 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -28,4 +28,5 @@ print(sample_df['var_5'])
 #print(sample_df['var_1', 'var_2']) #[] 안에는 하니의 값만 들어갈 수 있음
 print(sample_df[['var_1', 'var_4']]) #[ [] ] 를 사용하면 [] 가 하나의 값으로 인식팀
 
-#loc 는 location의  앞
+#loc 는 location의  앞글자
+print(sample_df.loc)
