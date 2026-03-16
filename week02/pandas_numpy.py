bc0fe6c8c0c617924c3f391391639eb46ac7d233
thetkomaung <thetkomaung966@gmail.com>
2026-03-17 01:56:53 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -58,4 +58,5 @@ print(less2015.head())
 
 print((netflix['release_year'] > 2015) & (netflix['type'] == 'TV Show'))
 
-more2015_tv = netflix[(netflix['release_year'] > 2015)]
+more2015_tv = netflix[(netflix['release_year'] > 2015) & (netflix['type'] == 'TV Show')]
+
