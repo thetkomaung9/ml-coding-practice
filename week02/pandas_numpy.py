ca908637d0d77ac669239207965120fcb098ee41
thetkomaung <thetkomaung966@gmail.com>
2026-03-18 02:24:06 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -116,7 +116,7 @@ print(iris['class'].value_counts())
 # 에제 데이터 생성
 data ={
     'name' : ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
-    'age': [25, 30, 28, 40],
+    'age': [25, 30, 35, 28, 40],
     'salary': [70000.00, 80000.00, 90000.00, 60000.00, 95000.00]
 }
 
