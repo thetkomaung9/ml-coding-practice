a3897afe5d9aeb1134d4c7c88c5d0b91b6cc668f
thetkomaung <thetkomaung966@gmail.com>
2026-03-18 01:52:36 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -3,7 +3,7 @@ import pandas as pd
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample.csv'
 sample = pd.read_csv(file_url)
 
-print(sample.head)
+print(sample.head())
 print(sample.tail())
 
 sample.inf0()
