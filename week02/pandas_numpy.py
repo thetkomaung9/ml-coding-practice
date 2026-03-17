100fc95f503902069d3904bc6dbd097e189f6d0d
thetkomaung <thetkomaung966@gmail.com>
2026-03-18 01:50:06 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -220,9 +220,9 @@ print(a.sum())          # 모든 요소의 합
 print(a.mean())         # 모든 요소의 평군
 print(a.mean(axis = 0))     # 욜을  기준으로 연산
 
-print(a.min())          # 
-print(a.max())
+print(a.min())          # 모든 요소 중 최솟값
+print(a.max())          # 모든 요소 중 최댓값
 
 print(a.max(axis=1))
-print(a.cumsum())
-print(a.argmax())
+print(a.cumsum())       # 모든 요소의 누적합
+print(a.argmax())       # 모든 요소 중 최댓값의 인덱스
