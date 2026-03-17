ee902859f8310d40531c49657ebb7776edb68d13
thetkomaung <thetkomaung966@gmail.com>
2026-03-18 02:20:06 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -104,7 +104,7 @@ print(iris.head())
 
 print(iris.groupby('class').std())
 
-print(iris.drop('class', 'axis=1').agg(['sum', 'mean', 'std']))
+print(iris.drop('class', axis=1).agg(['sum', 'mean', 'std']))
 
 # print(iris.agg(['sum', 'mean', 'std'])) #class 컴럼으로 인색 에러 박생
 
