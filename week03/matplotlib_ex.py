78d0f8144e04d16653a6e7c2dd0426701317332e
thetkomaung <thetkomaung966@gmail.com>
2026-03-27 01:34:02 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -19,4 +19,6 @@ print(titanic.info())
 pclass_survived_mean = titanic.groupby('Pclass') ['Survived'].mean().reset_index()
 pclass_survived_mean
 
-# 
+# 맷플롯립 라이브러리 불러오기  
+import matplotlib.pyplot as plt
+
