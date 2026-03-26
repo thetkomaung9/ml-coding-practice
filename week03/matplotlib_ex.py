90da811ba91ded89a65d0ff11aab1c50e972d9cf
thetkomaung <thetkomaung966@gmail.com>
2026-03-27 01:43:02 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -28,3 +28,4 @@ plt.plot(pclass_survived_mean['Pclass'], pclass_survived_mean['Survived'],
 plt.title('Survival Rate Variation Across Passenger Classes')
 plt.xlabel('Pclass')
 plt.ylabel('Survival Rate')
+plt.xticks([1, 2, 3])
