6d1e909c32248c5507154e9667f44ffa26c68c7e
thetkomaung <thetkomaung966@gmail.com>
2026-03-27 01:40:32 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -25,3 +25,4 @@ import matplotlib.pyplot as plt
 # 선 그래프 그리기
 plt.plot(pclass_survived_mean['Pclass'], pclass_survived_mean['Survived'],
          marker='o' , linestyle='-', color='violet')
+plt.title('Survival ')
