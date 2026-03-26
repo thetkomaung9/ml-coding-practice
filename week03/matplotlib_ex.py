2a2a284468d807c70e20616f7cc294184846069b
thetkomaung <thetkomaung966@gmail.com>
2026-03-27 02:21:02 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -89,7 +89,7 @@ print(titanic.info())
 
 # 산점도 그래프 그리기
 plt.figure(figsize=(12, 8)) 
-scatter = plt.scatter(x='Age', y='Fare', data=titanic, )   
+scatter = plt.scatter(x='Age', y='Fare', data=titanic, c=titanic[''])   
 
 
 
