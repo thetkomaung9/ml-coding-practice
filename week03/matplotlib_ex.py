c685d3806ca284ddd0d25cff94e41d29f9f22e8d
thetkomaung <thetkomaung966@gmail.com>
2026-03-27 02:29:32 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -108,6 +108,6 @@ print(survived_counts)
 # 파이 차트 그리기
 plt.figure(figsize=(8, 8))
 plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange', 'gold'],
-        
+        autopct='%1.1f%%', startangle=90, explode=[0.1, 0])
 
 
