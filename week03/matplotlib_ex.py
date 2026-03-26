84d7b3bca0b9ec756f098d4898359705a52ae2b2
thetkomaung <thetkomaung966@gmail.com>
2026-03-27 01:33:02 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -16,4 +16,7 @@ print(titanic.info())
 """### **선 그래프 : 객실 등급에 따른 생존율 표시하기**"""
 
 # 객실 등급에 따른 생존자와 사망자의 평균 계산
-pclass_survived_mean = titanic.groupby('Pclass')['Survived
+pclass_survived_mean = titanic.groupby('Pclass') ['Survived'].mean().reset_index()
+pclass_survived_mean
+
+# 
