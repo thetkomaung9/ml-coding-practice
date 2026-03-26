c3aab53b796cc4274e28296c56fdaa549399a0f0
thetkomaung <thetkomaung966@gmail.com>
2026-03-27 01:41:32 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -25,4 +25,5 @@ import matplotlib.pyplot as plt
 # 선 그래프 그리기
 plt.plot(pclass_survived_mean['Pclass'], pclass_survived_mean['Survived'],
          marker='o' , linestyle='-', color='violet')
-plt.title('Survival ')
+plt.title('Survival Rate Variation Across Passenger Classes')
+plt.xlabel('')
