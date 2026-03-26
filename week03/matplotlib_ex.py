07a5924e6c4df2c8a4db37c4ab07a41377080c49
thetkomaung <thetkomaung966@gmail.com>
2026-03-27 02:43:02 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -46,7 +46,7 @@ plt.title('Survived Counts by Embarked Port on Titanic')
 plt.xlabel('Embarked Port')
 plt.ylabel('Counts')
 plt.xticks(survived_counts.index, ['Southampton', 'Cherbourg', 'Queenstown'])
-plt.legend(['Survived']), loc=('upper right')
+plt.legend(['Survived'], loc='upper right')
 plt.gird(axis='y', linestyle='--', alpha=0.7)
 
 # 생존자 수 표시
