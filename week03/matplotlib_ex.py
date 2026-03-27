874137642de9554cacb1773c385cd24fe9fc5d4d
thetkomaung <thetkomaung966@gmail.com>
2026-03-28 02:05:25 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -229,4 +229,7 @@ plt.xticks([1, 2, 3], ['1st Class', '2nd Class', '3rd Class'])
 
 # 범례 설정
 plt.legend(violin_plot['bodies'], ['1st Class', '2nd Classs', '3rd Class'],
-           )
+           title='Pclass', loc='upper right')
+plt.savefig('Figures10.png')
+plt.close()
+
