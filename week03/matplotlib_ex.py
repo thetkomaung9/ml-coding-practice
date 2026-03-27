af140c8dc3a556d3a32f13262ebe0f21a93d66e0
thetkomaung <thetkomaung966@gmail.com>
2026-03-28 02:27:55 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -279,4 +279,5 @@ plt.close()
 # Survived가 0이면 사망자를, 1이면 생존자를 나타냄
 titanic = pd.read_csv('3.1.1.titanic.csv')
 
-#
+# 각 부모와 자녀의 수에 따른 생존자와 사망자 수 계산
+parch_counts = titanic.groupby(['Parch', 'Survived'], observed=False).size().unstack().fillna(0)
