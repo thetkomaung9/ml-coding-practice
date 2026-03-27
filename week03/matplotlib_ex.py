9ce7c79c9778b0431320737276c8e633f8e88ed8
thetkomaung <thetkomaung966@gmail.com>
2026-03-28 02:45:25 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -144,7 +144,7 @@ correlation_matrix = titanic.drop('PassengerId', axis=1).corr(numeric_only=True)
 print(correlation_matrix)
 
 # 히트맵 그리기
-plt.matshow(correlation_matrix, camp='PuRd_r')
+plt.matshow(correlation_matrix, cmap='PuRd_r')
 plt.colorbar()
 
 # x축과 y축의 눈금 설정
