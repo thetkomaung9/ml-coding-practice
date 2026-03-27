52a93385475d5dd1a1888efa1e9651e7853b4943
thetkomaung <thetkomaung966@gmail.com>
2026-03-28 02:18:25 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -251,3 +251,4 @@ plt.figure(figsize=(10, 6))
 plt.errorbar(fare_means.index, fare_means, yerr=fare_std, fmt='0',
              capsize=5, capthick=1, label='Fare')
 
+plt.title('Error Bar Plot of Fare by Parch')
