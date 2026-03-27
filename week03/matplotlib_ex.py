7259b019810016c5455dc0fa06e59c2bed714435
thetkomaung <thetkomaung966@gmail.com>
2026-03-28 01:36:25 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -164,4 +164,7 @@ titanic = titanic.dropna(subset=['Age', 'Fare'])
 age_groups = pd.cut(titanic['Age'], bins=range(0, 81, 5))
 
 # Age, Survived 기준으로 그룹화
-survived_counts = titanic.groupby([age_groups, 'Survived']).size().unstack(fill_value=0)
+survived_counts = titanic.groupby([age_groups, 'Survived'], observed=False).size().unstack().fillna(0)
+print(survived_counts)
+
+
