ff802cbf94574909dd74cdc7a256989f965c75b7
thetkomaung <thetkomaung966@gmail.com>
2026-03-28 01:42:55 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -175,4 +175,7 @@ plt.fill_between(survived_counts.index.astype(str), survived_counts[1],
                  color='purple', alpha=0.9, label='Survived')
 
 # 나이대별 사망자
-plt.fill_between()
+plt.fill_between(survived_counts.index.astype(str), survived_counts[0],
+                 color='hotpink', alpha=0.6, label='Not Survived')
+
+
