26944f34bcb8675cc4838887dc675ef9de363bd0
thetkomaung <thetkomaung966@gmail.com>
2026-03-28 01:21:55 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -140,5 +140,9 @@ plt.close()
 titanic = titanic.dropna(subset=['Age', 'Fare'])
 
 # 상관 행렬 계산
-correlation_matrix = titanic.drop()
+correlation_matrix = titanic.drop('PassengerId', axis=1).corr(numeric_only=True)
+print(correlation_matrix)
+
+# 히트맵 그리기
+
 
