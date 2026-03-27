993cdadca4fe64ff1e36b1d7ac1994610cec6726
thetkomaung <thetkomaung966@gmail.com>
2026-03-28 01:40:55 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -172,4 +172,7 @@ plt.figure(figsize=(10, 6))
 
 # 나이대별 생존자
 plt.fill_between(survived_counts.index.astype(str), survived_counts[1],
-                 color='purple',)
+                 color='purple', alpha=0.9, label='Survived')
+
+# 나이대별 사망자
+
