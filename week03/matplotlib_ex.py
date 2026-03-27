52f520e8535a961eb45713cb968f80b83646164b
thetkomaung <thetkomaung966@gmail.com>
2026-03-28 01:51:25 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -198,3 +198,10 @@ plt.boxplot([titanic[titanic['Pclass'] == 1]['Age'],
              titanic[titanic['Pclass'] == 2]['Age'],
              titanic[titanic['Pclass'] == 3]['Age']],
             labels=['1st Class', '2nd Class', '3rd Class'])
+
+plt.title('Box Plot for Age by Pclass')
+plt.xlabel('Pclass')
+plt.ylabel('Age')
+plt.savefig('Figures09.png')
+plt.close()
+
