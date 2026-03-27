d0631cb0241aebdb285b59c001a9f86bf487c4a8
thetkomaung <thetkomaung966@gmail.com>
2026-03-28 02:30:55 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -281,3 +281,8 @@ titanic = pd.read_csv('3.1.1.titanic.csv')
 
 # 각 부모와 자녀의 수에 따른 생존자와 사망자 수 계산
 parch_counts = titanic.groupby('Parch') ['Survived'].value_counts().unstack().fillna(0)
+print(parch_counts)
+
+# x, y 설정
+x = parch_counts.index.astype(str) # 데이터프레임의 인덱스를 문자열로 반환
+
