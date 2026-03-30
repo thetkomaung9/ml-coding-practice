7816b553d497ed119a6d979ec701d737f6a65f3c
thetkomaung <thetkomaung966@gmail.com>
2026-03-31 00:39:29 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -11,7 +11,7 @@ def load_housing_data():
     tarball_path = Path("datasets/housing.tgz") 
     if not tarball_path.is_file():
         Path("datasets").mkdir(parents=True, exist_ok=True)
-        url = "https://raw.github.com/ageron/data/raw/main/housing.tgz"
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
         urllib.request.urlretrieve(url, tarball_path)
         with tarfile.open(tarball_path) as housing_tarball:
             housing_tarball.extractall(path="datasets")
