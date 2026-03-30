d6396ff94db80bc65f5cc5f220ca7c2954aa2ac7
thetkomaung <thetkomaung966@gmail.com>
2026-03-31 00:07:58 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -27,5 +27,5 @@ housing["income_cat"] = pd.cut(housing["median_income"],
                                labels=[1, 2, 3, 4, 5])
 
 strat_train_set, strat_test_set = train_test_split(
-    
-)
+    housing, test_size=0.2, stractify=housing["income_cat"], random_state=42)
+
