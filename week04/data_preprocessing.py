53106af5e07b1fce34e4bbc35626da386f83a608
thetkomaung <thetkomaung966@gmail.com>
2026-03-31 00:01:28 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -7,4 +7,7 @@ import pandas as pd
 import tarfile
 import urllib.request
 
-def load_housing_data
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz") 
+    if not tarball_path.is_file():
+        Path()
