76adb625cf777b4a99098248a89d06ea760c2afa
thetkomaung <thetkomaung966@gmail.com>
2026-04-03 01:41:04 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -37,19 +37,19 @@ from sklearn.impute import SimpleImputer
     
 imputer = SimpleImputer(strategy="median")
     
-    # 수치형 특성만 추출
-    housing_num = housing.select_dtypes(include=[np.number])
-    housing_num.head()
+# 수치형 특성만 추출
+housing_num = housing.select_dtypes(include=[np.number])
+housing_num.head()
     
-    imputer.fit(housing_num)
+imputer.fit(housing_num)
     
-    print(imputer.statistics_)        # imputer 결과 값
-    print(housing_num.median().values) # 수동으로 계산한 중간값
+print(imputer.statistics_)        # imputer 결과 값
+print(housing_num.median().values) # 수동으로 계산한 중간값
     
-    # 훈련 세트의 누락값을 imputer가 학습한 값으로 체우기
-    X = imputer.transform(housing_num)
+# 훈련 세트의 누락값을 imputer가 학습한 값으로 체우기
+X = imputer.transform(housing_num)
     
-    imputer.feature_names_in_
+imputer.feature_names_in_
     
     housing_tr = pd.DataFrame(X, columns=housing_num.columns,
                               index=housing_num.index)
