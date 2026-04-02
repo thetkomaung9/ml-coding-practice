5d07a01082c26a59801a24a0a04bca8219ea9326
thetkomaung <thetkomaung966@gmail.com>
2026-04-03 01:40:35 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -28,14 +28,14 @@ for set_ in (strat_train_set, strat_test_set):
 housing = strat_train_set.drop("median_house_value", axis=1) 
 housing_labels = strat_train_set["median_house_value"].copy()
     
-    # 데이터 정제
-    # null 값이 있는 행 확인하기
-    null_rows_idx = housing.isnull().any(axis=1)
-    housing.loc[null_rows_idx].head()
+# 데이터 정제
+# null 값이 있는 행 확인하기
+null_rows_idx = housing.isnull().any(axis=1)
+housing.loc[null_rows_idx].head()
     
-    from sklearn.impute import SimpleImputer
+from sklearn.impute import SimpleImputer
     
-    imputer = SimpleImputer(strategy="median")
+imputer = SimpleImputer(strategy="median")
     
     # 수치형 특성만 추출
     housing_num = housing.select_dtypes(include=[np.number])
