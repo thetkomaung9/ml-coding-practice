7e005dbc9b79d293b2ba49e601b860a5a14961c9
thetkomaung <thetkomaung966@gmail.com>
2026-04-03 01:25:04 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -7,17 +7,6 @@ import pandas as pd
 import tarfile
 import urllib.request
 
-def load_housing_data():
-    tarball_path = Path("datasets/housing.tgz") 
-    if not tarball_path.is_file():
-        Path("datasets").mkdir(parents=True, exist_ok=True)
-        url = "https://github.com/ageron/data/raw/main/housing.tgz"
-        urllib.request.urlretrieve(url, tarball_path)
-        with tarfile.open(tarball_path) as housing_tarball:
-            housing_tarball.extractall(path="datasets")
-    return pd.read_csv(Path("datasets/housing/housing.csv"))
-
-housing = load_housing_data()
 
 # 테스트 세트 만들기
 from sklearn.model_selection import train_test_split
