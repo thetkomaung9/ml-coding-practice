3e200b44e78179436cfc0ca08d70c7f2b5d5ae66
thetkomaung <thetkomaung966@gmail.com>
2026-04-03 01:26:34 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -3,9 +3,8 @@
 # 데이터 준비
 import numpy as np
 import pandas as pd
-import tarfile
-import urllib.request
 
+housing = pd.read_csv('./week04/housing.csv') # 우류 발생 시, ./housing.csv 파일 로도 시도
 
 # 테스트 세트 만들기
 from sklearn.model_selection import train_test_split
