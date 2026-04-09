d7ebc2027fe288fdca3b9789778571fc694ac0dd
thetkomaung <thetkomaung966@gmail.com>
2026-04-10 02:20:00 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -28,4 +28,10 @@ def main():
     print('전체 검색 : %d 건' %total)
     
     with open('%s_naver_%s.json' % (srcText, node), 'w', encoding = 'utf8') as outfile:
-        jsonFile = 
+        jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
+                              ensure_ascii = False)
+        
+        outfile.write(jsonFile)
+
+    print("가져온 데이터 : %d 건" %(cnt))
+    print('%s_naver_%s.json ')
