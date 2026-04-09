97b98a6a7567daefde3ef3aec68b9d3af19680c5
thetkomaung <thetkomaung966@gmail.com>
2026-04-10 02:04:00 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -14,4 +14,7 @@ def main():
     cnt = 0
     jsonResult = []
     
-    jsonResponse = getNaverSearch(node, srcText, 1, 100)
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)    # [CODE 2]
+    total = jsonResponse['total']
+    
+    while ((jsonResponse !=None) and (jsonResponse['display']))
