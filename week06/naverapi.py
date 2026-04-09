ed1bed90453e30895dffcb9cb3ae4935e6ee1404
thetkomaung <thetkomaung966@gmail.com>
2026-04-10 02:45:31 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -77,4 +77,14 @@ def getPostData(post, jsonResult, cnt):  #[CODE 3]
     # %a: 짧은 형식의 요일 이름 (예, 'Mon', "True', "Wed', ...)
     # %d: 일 (에, 01, 02, 03, ...)
     # %b: 짧은 형식의 월 이름 (예, 'Jan', 'Feb', 'Mar', ...)
-    pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S')
+    pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S+0900')
+    pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
+    
+    jsonResult.append({
+        'cnt' : cnt,
+        'title' : title,
+        'description' : description,
+        'org_link' : org_link
+        'link' : link,
+        'pDate' : pDate
+    })
