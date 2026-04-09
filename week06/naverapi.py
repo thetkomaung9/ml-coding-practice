35bf08a7207c04c7a622e9e294702e1e61ca79c0
thetkomaung <thetkomaung966@gmail.com>
2026-04-10 02:33:00 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -54,4 +54,12 @@ def getNaverSearch(node, srcText, page_start, display):
 def getRequestUrl(url):
     req = urllib.request.Request(url) 
     
-    req.add_header("") 
+    req.add_header("X-Naver-Client-Id", client_id)
+    req.add_header("X-Naver-Client_Secret", client_secret)
+    
+    try:
+        response = urllib.request.urlopen(req) 
+        if response.getcode() == 200:
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+        
