28e8464fe6f7bdf3e59c6ba73f603b9125b25cc3
thetkomaung <thetkomaung966@gmail.com>
2026-04-10 02:08:30 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+import urllib.request
+import datetime
+import json
+
+client_id = 'Client ID'
+client_secret = 'Client Secret'
+
+def main():
+    
+    node = 'news'
+    srcText = input ('검색어를 입력하세요: ')
+    
+    cnt = 0
+    jsonResult = []
+    
