274e9e1c3cf399d4eaa00f06b8eaa73107b07a83
thetkomaung <thetkomaung966@gmail.com>
2026-04-10 02:24:30 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -40,5 +40,5 @@ def main():
 def getNaverSearch(node, srcText, page_start, display):
     base = "https://openai.naver.com/v1/search"
     node = "/%s.json" % node
-    parameters = "?query=%s&start"
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText),)
     
