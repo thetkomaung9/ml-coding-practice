bc853d1f6ab53d2c420eef1b7b04bbfec4632cc4
thetkomaung <thetkomaung966@gmail.com>
2026-04-10 02:50:30 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -84,7 +84,7 @@ def getPostData(post, jsonResult, cnt):  #[CODE 3]
         'cnt' : cnt,
         'title' : title,
         'description' : description,
-        'org_link' : org_link
+        'org_link' : org_link,
         'link' : link,
         'pDate' : pDate
     })
