93596f83ccaca1c05154897475b22316cb1a56c2
thetkomaung <thetkomaung966@gmail.com>
2026-04-10 23:56:28 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -34,4 +34,4 @@ def getTourismStatesService(nat_cd, ed_cd, nStartYear, nEndYear):
     for year in range(nStartYear, nEndYear+1):
         for month in range(1, 13):
             yyyymm = "{0}{1:0>2}".format(str(year), str(month))
-            json
+            jsonData = getTourismStatesItem()
