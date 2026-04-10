81d6a11791e8da2b7ecef5e554d09849cba9ba4b
thetkomaung <thetkomaung966@gmail.com>
2026-04-11 00:18:28 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -80,4 +80,9 @@ def getRequestUrl(url): #[CODE 1]
     try:
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
-            print("[]")
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
