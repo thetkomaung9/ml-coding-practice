998854d5286b72e5c2b4a02c1c0c08c7098e8777
thetkomaung <thetkomaung966@gmail.com>
2026-05-10 02:12:43 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -25,6 +25,8 @@ print(svm_clf.decision_function(X_new))
 from sklearn.datasets import make_moons
 from sklearn.preprocessing import PolynomialFeatures
 
+X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
+
 
 
 
