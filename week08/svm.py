0b2819fa66af95c8ccc756f0c39c739f3606bced
thetkomaung <thetkomaung966@gmail.com>
2026-05-10 02:33:14 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -68,6 +68,7 @@ X = 2 * np.random.rand(50, 1) - 1
 y = 0.2 + 0.1 * X[:, 0] + 0.5 * X[:, 0] ** 2 + np.random.randn(50) / 10
 
 svm_poly_reg = make_pipeline(StandardScaler(),
-                             SVR(kernel="poly", degree=2, ))
+                             SVR(kernel="poly", degree=2, C=0.01, epsilon=0.1))
+svm_poly_reg.fit(X, y)
 
 
