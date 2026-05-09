fe98c80a0cddc5f92fb9a34a397b1f053984ff4f
thetkomaung <thetkomaung966@gmail.com>
2026-05-10 02:15:14 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -30,7 +30,8 @@ X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
 polynomial_svm_clf = make_pipeline(
     PolynomialFeatures(degree=3),
     StandardScaler(),
-    LinearSVC(C=10, max_iter=10_000,)
+    LinearSVC(C=10, max_iter=10_000, dual=True, random_state=42)
 )
+
 
 
