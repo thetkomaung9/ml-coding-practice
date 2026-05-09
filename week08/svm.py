86f13ca3843c86ecfa045a89784791501052f8a5
thetkomaung <thetkomaung966@gmail.com>
2026-05-10 02:14:14 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -27,6 +27,10 @@ from sklearn.preprocessing import PolynomialFeatures
 
 X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
 
-polynomial_svm_clf =
+polynomial_svm_clf = make_pipeline(
+    PolynomialFeatures(degree=3),
+    StandardScaler(),
+    
+)
 
 
