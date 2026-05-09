81e1e939f2fe358bc84daebd24f3003e1d414248
thetkomaung <thetkomaung966@gmail.com>
2026-05-10 02:23:44 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -45,6 +45,8 @@ poly_kernel_svm_clf.fit(X, y)
 # 가우스 RBF 커널
 rbf_kernel_svm_clf = make_pipeline(StandardScaler(),
                                    SVC(kernel="rbf", gamma=5, C=0.001))
+rbf_kernel_svm_clf.fit(X, y)
 
+# 
 
 
