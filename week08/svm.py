461ba4dd41c60346463c6cd6198fe51a7b49b5ff
thetkomaung <thetkomaung966@gmail.com>
2026-05-10 02:04:43 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -2,4 +2,8 @@
 # 선형 SVM 분류
 
 import numpy as np
-import sklearn.datasets import load_iris
+from sklearn.datasets import load_iris
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
