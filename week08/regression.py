4a2892606afae3508259fc6656d4c2c210ef8238
thetkomaung <thetkomaung966@gmail.com>
2026-05-11 04:24:16 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -3,3 +3,10 @@ import matplotlib.pyplot as plt
 
 plt.rc('font', size=14)
 plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+
+import numpy as np
+
+np.random.seed(42)
