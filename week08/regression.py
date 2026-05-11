0f364974f9c86d9541328e811dfb00957b1c301c
thetkomaung <thetkomaung966@gmail.com>
2026-05-12 00:20:14 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -17,4 +17,11 @@ y = 4 + 3 * X + np.random.randn(m, 1)
 import matplotlib.pyplot as plt
 
 plt.figure(figsize=(6, 4))
-plt.
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$")
+plt.ylabel("$y$", rotation=0)
+plt.axis([0, 2, 0, 15])
+plt.grid()
+plt.show()
+
+from
