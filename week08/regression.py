2320a254cf7163f8d4303bffaaf3694647c1a182
thetkomaung <thetkomaung966@gmail.com>
2026-05-12 00:46:14 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -133,6 +133,9 @@ elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
 elastic_net.fit(X, y)
 print(elastic_net.predict([[1.5]]))
 
+# 로지스틱 회귀
+from sklearn.datasets import load_iris
 
+iris = load_iris
 
                        
