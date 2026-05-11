f3db66777681ca8fa8008420a72552055f7f435f
thetkomaung <thetkomaung966@gmail.com>
2026-05-12 00:51:14 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -151,5 +151,6 @@ X = iris.data[["petal width (cm)"]].values
 y = iris.target_names[iris.target] == 'virginica'
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-log_reg = LogisticRegression(solver="lbfgs)
+log_reg = LogisticRegression(random_state=42)
+
                        
