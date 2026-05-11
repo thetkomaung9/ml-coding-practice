c690fdbfb95f19ab6a993d320e40c2563c802aa7
thetkomaung <thetkomaung966@gmail.com>
2026-05-12 00:38:14 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -108,5 +108,5 @@ print(ridge_reg.predict([[1.5]]))
 
 sgd_reg = SGDRegressor(penalty="l2", alpha=0.1 / m, tol=None,
                        max_iter=1000, eta0=0.1, random_state=42)
-
+sgd_reg.fit(X, y.ravel()) # fit()은 1D 
                        
