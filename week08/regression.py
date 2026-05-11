04d91b6d356899956887c72e50968801e0953cba
thetkomaung <thetkomaung966@gmail.com>
2026-05-12 00:53:14 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -160,5 +160,5 @@ print(log_reg.predict([[1.7], [1.5]]))
 
 X = iris.data[["petal length (cm)", "petal width (cm)"]].values
 y = iris["target"]
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
                        
