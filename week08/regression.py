47eadd80acadc466a334074831db51caf1969242
thetkomaung <thetkomaung966@gmail.com>
2026-05-12 00:36:44 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -104,4 +104,7 @@ from sklearn.linear_model import Ridge
 
 ridge_reg = Ridge(alpha=0.1, solver="cholesky") 
 ridge_reg.fit(X, y)
-print(ridge_reg.predict([[1.5]]))        
+print(ridge_reg.predict([[1.5]]))
+
+sgd_reg = SGDRegressor(penalty="l2", alpha=0.1,
+                       )        
