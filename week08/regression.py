485942718ebed334fb8e5ed5a7adbee48516b72c
thetkomaung <thetkomaung966@gmail.com>
2026-05-12 00:30:14 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -75,4 +75,6 @@ y_new = lin_reg.predict(X_new_poly)
 
 plt.figure(figsize=(6, 4))
 plt.plot(X, y, "b.")
-plt.plot(X_new, y_new, "r-", linewidth=2, label="Predictions)
+plt.plot(X_new, y_new, "r-", linewidth=2, label="Predictions")
+
+         
