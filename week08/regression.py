3f92e5777af8f8f69f90a584b31c82cc8836f96d
thetkomaung <thetkomaung966@gmail.com>
2026-05-12 00:37:44 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -106,5 +106,7 @@ ridge_reg = Ridge(alpha=0.1, solver="cholesky")
 ridge_reg.fit(X, y)
 print(ridge_reg.predict([[1.5]]))
 
-sgd_reg = SGDRegressor(penalty="l2", alpha=0.1, 
+sgd_reg = SGDRegressor(penalty="l2", alpha=0.1 / m, tol=None,
+                       max_iter=1000, eta0=0.1, random_state=42)
+
                        
