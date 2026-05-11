8c76e44ca598f7aa6fc70a49435acdd835762558
thetkomaung <thetkomaung966@gmail.com>
2026-05-12 00:54:14 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -161,4 +161,7 @@ print(log_reg.predict([[1.7], [1.5]]))
 X = iris.data[["petal length (cm)", "petal width (cm)"]].values
 y = iris["target"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+softmax_reg = LogisticRegression(C=30, random_state=42)
+
                        
