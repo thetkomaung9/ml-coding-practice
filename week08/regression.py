3c2a6d649dd15f1549a3fc8c4e137f2f9aa04478
thetkomaung <thetkomaung966@gmail.com>
2026-05-12 00:49:14 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -147,5 +147,5 @@ print(iris.target_names)
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-X = 
+X = iris.data[[]]
                        
