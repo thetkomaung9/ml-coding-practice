d5a49ab375a8bf0a79726f13abfbc4836a3b664b
thetkomaung <thetkomaung966@gmail.com>
2026-05-12 00:26:14 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -48,4 +48,8 @@ sgd_reg.intercept_, sgd_reg.coef_
 np.random.seed(42)
 m = 100
 X = 6 * np.random.rand(m, 1) - 3
-y = 0.5 * X**2 + X + 2 + np.random.randn
+y = 0.5 * X**2 + X + 2 + np.random.randn(m, 1)
+
+plt.figure(figsize=(6, 4))
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$")
