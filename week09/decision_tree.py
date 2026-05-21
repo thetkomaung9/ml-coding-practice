1453960d253920a97b8f954ebdaee84293b18d2d
thetkomaung <thetkomaung966@gmail.com>
2026-05-22 02:54:17 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -94,4 +94,4 @@ y_pred = tree_clf_pca.predict(X_iris_pca_all).reshape(z0s.shape)
 plt.contourf(z0s, z1s, y_pred, alpha=0.3, cmap=custom_cmap)
 for idx, (name, style) in enumerate(zip(iris.target_names, ("yo", "bs", "g^"))):
     plt.plot(X_iris_rotated[iris.target == idx, 0],
-             
+             X_iris_rotated[]
