4b617b701c70e3d1a1320b0832b67144953ec3a8
thetkomaung <thetkomaung966@gmail.com>
2026-05-22 03:03:47 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -101,3 +101,4 @@ plt.xlabel("$z_1$")
 plt.ylabel("$z_2$", rotation=0)
 th1, th2 = tree_clf_pca.tree_.threshold[[0, 2]]
 plt.plot([th1, th1], axes[2:], "k--", linewidth=2)
+plt.plot([th2, th2], axes[2:], "k--", linewidth=2)
