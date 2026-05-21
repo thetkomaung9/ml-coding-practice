daf10f55881d92f09a39e4ea0dddbd00932e9898
thetkomaung <thetkomaung966@gmail.com>
2026-05-22 02:19:47 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -5,3 +5,9 @@ iris = load_iris(as_frame=True)
 X_iris = iris.data[["petal length (cm)", "petal width (cm)"]].values
 y_iris = iris.target
 
+tree_clf = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree_clf.fit(X_iris, y_iris)
+
+from sklearn.tree import export_graphviz
+
+export_graphviz(
