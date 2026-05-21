1380a3259028422a02e939232d37b024f9faa390
thetkomaung <thetkomaung966@gmail.com>
2026-05-22 02:50:47 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -92,3 +92,4 @@ X_iris_pca_all = np.c_[z0s.ravel(), z1s.ravel()]
 y_pred = tree_clf_pca.predict(X_iris_pca_all).reshape(z0s.shape)
 
 plt.contourf(z0s, z1s, y_pred, alpha=0.3, cmap=custom_cmap)
+for idx, (name, style) in enumerate(zip(iris.target))
