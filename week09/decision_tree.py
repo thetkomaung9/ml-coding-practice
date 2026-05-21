c6c17e35d2ec23dc7ffac014e0cba3746f7ff7d9
thetkomaung <thetkomaung966@gmail.com>
2026-05-22 02:26:47 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -35,4 +35,5 @@ from sklearn.datasets import make_moons
 X_moons, y_moons = make_moons(n_samples=150, noise=0.2, random_state=42)
 
 tree_clf2 = DecisionTreeClassifier(random_state=42)
-tree_clf2 = De
+tree_clf2 = DecisionTreeClassifier(min_samples_leaf=5, random_state=42)
+tr
