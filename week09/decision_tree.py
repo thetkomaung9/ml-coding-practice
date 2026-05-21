4a298e910b18dd9dcc37eaa28ba53fd47c144c66
thetkomaung <thetkomaung966@gmail.com>
2026-05-22 02:20:47 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -11,3 +11,12 @@ tree_clf.fit(X_iris, y_iris)
 from sklearn.tree import export_graphviz
 
 export_graphviz(
+    tree_clf,
+    out_file="iris_tree.dot",
+    feature_names=["petal length (cm)", "petal width (cm)"],
+    class_names=iris.target_names,
+    rounded=True,
+    filled=True
+)
+
+from graphviz import Source
