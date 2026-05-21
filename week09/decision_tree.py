ba936436129fcfb7fe5383210365bfe2336b2b7c
thetkomaung <thetkomaung966@gmail.com>
2026-05-22 02:41:17 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -77,4 +77,4 @@ X_iris_rotated = pca_pipeline.fit_transform(X_iris)
 tree_clf_pca = DecisionTreeClassifier(max_depth=2, random_state=42)
 tree_clf_pca.fit(X_iris_rotated, y_iris)
 
-# 
+# 추가 코드 - 이 셀은 그림 6-8을 생성하고 저장합니다.
