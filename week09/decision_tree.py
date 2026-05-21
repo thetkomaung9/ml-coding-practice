dda622759f7d5766699ee491248c02b72775c5df
thetkomaung <thetkomaung966@gmail.com>
2026-05-22 02:57:47 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -97,4 +97,4 @@ for idx, (name, style) in enumerate(zip(iris.target_names, ("yo", "bs", "g^"))):
              X_iris_rotated[:, 1][y_iris == idx],
              style, label=f"Iris {name}")
 
-             
+plt.xlabel()             
