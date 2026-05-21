fe43b55475626e5a2b705c28b5ebdd4061531ce1
thetkomaung <thetkomaung966@gmail.com>
2026-05-22 03:02:17 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -100,3 +100,4 @@ for idx, (name, style) in enumerate(zip(iris.target_names, ("yo", "bs", "g^"))):
 plt.xlabel("$z_1$")
 plt.ylabel("$z_2$", rotation=0)
 th1, th2 = tree_clf_pca.tree_.threshold[[0, 2]]
+plt.plot([th1, th1],)
