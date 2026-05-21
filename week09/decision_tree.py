de861dc760ee5370c44eb33cd051df41b673b87a
thetkomaung <thetkomaung966@gmail.com>
2026-05-22 02:45:47 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -88,4 +88,3 @@ plt.figure(figsize=(8, 4))
 axes = [-2.2, 2.4, -0.6, 0.7]
 z0s, z1s = np.meshgrid(np.linespace(axes[0], axes[1], 100,
                        np.linespace(axes[2], axes[3], 100))
-                       )
