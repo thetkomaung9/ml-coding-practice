812d748d32bab1a25c19851b9c75e3bf36763ccd
thetkomaung <thetkomaung966@gmail.com>
2026-05-22 02:42:47 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -81,4 +81,5 @@ tree_clf_pca.fit(X_iris_rotated, y_iris)
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
-custom_cmap = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+custom_cmap = ListedColormap(["#fafab0", "#9898ff", "#a0faa0"])
+
