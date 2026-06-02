e1697f986a0a54ef364d7df64eb75eefc1b40944
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 02:55:57 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -140,4 +140,5 @@ def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
     non_cores = X[non_core_mask]
     
     plt.scatter(cores[:, 0], cores[:, 1], c=dbscan.labels_[core_mask], marker='o', s=size, cmap="Paired")
-    plt.scatter(cores[:, 0], cores[:, 1], marker='*', s=20, )
+    plt.scatter(cores[:, 0], cores[:, 1], marker='*', s=20, c=dbscan.labels_[core_mask])
+    plt.
