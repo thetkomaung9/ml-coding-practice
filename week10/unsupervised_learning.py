f78659d811c0671738ee543aebdea5439c30139f
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 02:40:26 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -97,4 +97,7 @@ for k in (3, 4, 5, 6):
         color = plt.cm.Spectral(i / k)
         plt.fill_betweenx(np.arange(pos, pos + len(coeffs)), 0, coeffs, 
                           facecolor=color, edgecolor=color, alpha=0.7)
+        ticks.append(pos + len(coeffs) // 2)
+        pos += len(coeffs) + padding
         
+    plt.gca().yaxis.set_major_locator(FixedLocator(ticks))
