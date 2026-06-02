83d5fa92a0855cf474fddae10c48260de5012c37
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 02:04:26 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -2,4 +2,6 @@
 import matplotlib.pyplot as plt
 
 plt.rc('font', family='Malgun Gothic') # 원도우: 맑은 고딕
-plt.rcParams['axes.unicode_minus'] = False # 
+plt.rcParams['axes.unicode_minus'] = False # 마이너스(-) 기호 깨짐 방지
+
+plt.rc('font', size=14)
