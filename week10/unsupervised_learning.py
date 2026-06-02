e798d35163b0acd9d1646db55118d130ed6a1eae
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 02:09:56 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -17,3 +17,4 @@ import numpy as np
 
 blobs_centers = np.array([[ 0.2, 2.3], [-1.5, 2.3], [-2.8, 2.8],
                          [-2.8, 4.2], [-2.8, 1.0]])
+blob_std = np.array([0.4, 0.3, 0.1, 0.1, 0.1])
