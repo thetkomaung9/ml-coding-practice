6c3c253586b7542f03a06a13ca8cdedef86d9238
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 03:12:26 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -179,3 +179,4 @@ knn.fit(dbscan.components_, dbscan.labels_[dbscan.core_sample_indices_])
 
 X_new = np.array([[-0.5, 0], [0, 0.5], [1, -0.1], [2, 1]])
 print(knn.predict(X_new))
+print(knn.predict_proba(X_new))
