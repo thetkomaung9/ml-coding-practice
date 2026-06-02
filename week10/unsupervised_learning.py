d7545963a516a2827b9e34ffa9d1b5ea1ed36770
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 02:49:26 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -129,4 +129,5 @@ print(dbscan.labels_[:10])
 print(dbscan.core_sample_indices_[:10])
 print(dbscan.components_)
 
-def plot_dbscan(dbscan, X, size, show_cl)
+def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
+    
