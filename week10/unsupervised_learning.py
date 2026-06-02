dcaf67343a0cf9ff9e00ab82a05f491d78ff565f
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 02:35:27 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -82,4 +82,7 @@ from matplotlib.ticker import FixedLocator, FixedFormatter
 plt.figure(figsize=(11, 9))
 
 for k in (3, 4, 5, 6):
-    plt
+    plt.subplot(2, 2, k - 2)
+    
+    y_pred = kmeans_per_k[k - 1].labels_
+    
