af3b872a467141f4000dc2a13d3e5a10829b1928
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 03:04:26 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -153,4 +153,5 @@ def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
         plt.tick_params(labelleft=False)
     plt.title(f"eps={dbscan.eps:.2f}, min_samples={dbscan.min_samples}")
     plt.grid()
+    plt.gca().set_axisbelow(True)
     
