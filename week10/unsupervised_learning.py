8524a6bb8688c3bbf25941a12e6280e3a78fad3c
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 02:50:26 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -130,4 +130,5 @@ print(dbscan.core_sample_indices_[:10])
 print(dbscan.components_)
 
 def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
-    core_mask = np.zeros_like(dbscan)
+    core_mask = np.zeros_like(dbscan.labels_, dtype=bool)
+    core_mask
