ab4c2eba215da04def595c2d5008fa4c375aff50
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 02:44:26 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -112,4 +112,6 @@ for k in (3, 4, 5, 6):
         plt.tick_params(labelbottom=False)
         
     plt.axvline(x=silhouette_scores[k - 2], color="red", linestyle="--")
+    plt.title(f"$={k}$")
     
+plt.show()
