9fe6c2e189ea22cd43fb660f8f17e1913f3ad398
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 02:10:56 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -20,3 +20,5 @@ blobs_centers = np.array([[ 0.2, 2.3], [-1.5, 2.3], [-2.8, 2.8],
 blob_std = np.array([0.4, 0.3, 0.1, 0.1, 0.1])
 X, y = make_blobs(n_samples=2000, centers=blobs_centers, cluster_std=blob_std,
                    random_state=7)
+
+k = 5
