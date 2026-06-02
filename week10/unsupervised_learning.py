49c97f1560a1c214358e7321162feb5d7764d299
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 02:45:56 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -115,3 +115,10 @@ for k in (3, 4, 5, 6):
     plt.title(f"$={k}$")
     
 plt.show()
+
+# DBSCAN
+from sklearn.cluster import DBSCAN
+from sklearn.datasets import make_moons
+
+X, y = make_moons(n_samples=1000, noise=0.05, random_state=42)
+dbscan = DBSCAN(eps=0.05, min_samples=5)
