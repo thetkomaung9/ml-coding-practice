2a3620101ab50fb3330a6325126bdb2dba990391
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 02:17:26 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -30,5 +30,9 @@ def plot_clusters(X, y=None):
     plt.xlabel("$x_1$")
     plt.ylabel("$x_2$", rotation=0)
     
-plot.figure(figsize=(8, 4))
+plt.figure(figsize=(8, 4))
 plot_clusters(X)
+plt.gca().set_axisbelow(True)
+plt.grid()
+plt.show()
+
