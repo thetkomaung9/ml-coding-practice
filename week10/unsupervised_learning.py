7254b16ffb539347ecf6b71b2c20ef38aff12161
thetkomaung <thetkomaung966@gmail.com>
2026-06-03 03:05:56 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -155,3 +155,9 @@ def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
     plt.grid()
     plt.gca().set_axisbelow(True)
     
+dbscan2 = DBSCAN(eps=0.2)
+dbscan2.fit(X)
+
+plt.figure(figsize=(9, 3.2))
+
+plt.subplot(121)
